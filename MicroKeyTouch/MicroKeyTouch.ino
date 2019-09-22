1f95ad683bf4ccb74f08d513d017248b16081087
Salvador Rueda <salvador.rueda@gmail.com>
2019-09-22 22:17:23 +0200

    Cleaning code

--- MicroKeyTouch/MicroKeyTouch.ino
+++ MicroKeyTouch/MicroKeyTouch.ino
@@ -29,6 +29,9 @@ int16_t PENRADIUS = 1;
 uint16_t ID, oldcolor, currentcolor;
 uint8_t Orientation = 0;    //PORTRAIT
 
+boolean blocked = true;
+
+/*
 // Assign human-readable names to some common 16-bit color values:
 #define BLACK   0x0000
 #define BLUE    0x001F
@@ -38,124 +41,123 @@ uint8_t Orientation = 0;    //PORTRAIT
 #define MAGENTA 0xF81F
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
+*/
 
-void show_Serial(void)
-{
-    Serial.println(F("Most Touch Screens use pins 6, 7, A1, A2"));
-    Serial.println(F("But they can be in ANY order"));
-    Serial.println(F("e.g. right to left or bottom to top"));
-    Serial.println(F("or wrong direction"));
-    Serial.println(F("Edit name and calibration statements\n"));
-    Serial.println(name);
-    Serial.print(F("ID=0x"));
-    Serial.println(ID, HEX);
-    Serial.println("Screen is " + String(tft.width()) + "x" + String(tft.height()));
-    Serial.println("Calibration is: ");
-    Serial.println("LEFT = " + String(TS_LEFT) + " RT  = " + String(TS_RT));
-    Serial.println("TOP  = " + String(TS_TOP)  + " BOT = " + String(TS_BOT));
-    Serial.println("Wiring is always PORTRAIT");
-    Serial.println("YP=" + String(YP)  + " XM=" + String(XM));
-    Serial.println("YM=" + String(YM)  + " XP=" + String(XP));
+const uint16_t BLACK = 0x0000;
+const uint16_t RED = 0xF800;
+const uint16_t YELLOW = 0xFFE0;
+const uint16_t CYAN = 0x07FF;
+
+
+void button(){
+  uint16_t cxpos = 20;
+  uint16_t cypos = 280;
+  uint16_t cradius = 20; 
+  uint16_t ccolor = CYAN;
+  
+  tft.fillRect(cxpos, cypos-20, 100, 41, ccolor);
+  tft.fillCircle(cxpos, cypos, cradius, ccolor);
+  cxpos = cxpos + 100;
+  tft.fillCircle(cxpos, cypos, cradius, ccolor);
+  tft.setCursor(cxpos - 100, cypos);
+  tft.setTextSize(2);
+  tft.setTextColor(BLACK);
+  tft.print(F("Password"));  
 }
 
-void show_tft(void)
-{
-/*
-    tft.setCursor(0, 0);
-    tft.setTextSize(1);
-    tft.print(F("ID=0x"));  
-    tft.println(ID, HEX);
-    tft.println("Screen is " + String(tft.width()) + "x" + String(tft.height()));
-    tft.println("");
-    tft.setTextSize(2);
-    tft.println(name);
-    tft.setTextSize(1);
-    tft.println("PORTRAIT Values:");
-    tft.println("LEFT = " + String(TS_LEFT) + " RT  = " + String(TS_RT));
-    tft.println("TOP  = " + String(TS_TOP)  + " BOT = " + String(TS_BOT));
-    tft.println("\nWiring is: ");
-    tft.println("YP=" + String(YP)  + " XM=" + String(XM));
-    tft.println("YM=" + String(YM)  + " XP=" + String(XP));
-    tft.setTextSize(2);
-    
-  //  tft.setTextColor(RED);
-  //  tft.setCursor((tft.width() - 48) / 2, (tft.height() * 2) / 4);
-  //  tft.print("EXIT");
-    tft.setTextColor(YELLOW, BLACK);
-    tft.setCursor(0, (tft.height() * 6) / 8);
-    tft.print("Touch screen for loc");
+void button_exit(){
+  tft.setTextSize(2);
+  tft.setTextColor(RED);
+  tft.setCursor((tft.width() - 48) / 2, (tft.height() * 2) / 4);
+  tft.print("EXIT");
+  
+}
 
-  */  
-    while (1) {
-        tp = ts.getPoint();
-        pinMode(XM, OUTPUT);
-        pinMode(YP, OUTPUT);
-        if (tp.z < MINPRESSURE || tp.z > MAXPRESSURE) continue;
-        //if (tp.x > 450 && tp.x < 570  && tp.y > 450 && tp.y < 570) break;
-        //tft.setCursor(0, (tft.height() * 3) / 4);
-        tft.setCursor(0, 300);
-        //tft.print("tp.x=" + String(tp.x) + " tp.y=" + String(tp.y) + "   ");
-        if (tp.x > 450 && tp.x < 870  && tp.y > 750 && tp.y < 845){
-          //tft.print("Salvador Rueda");
-          Keyboard.println("Your text here"); 
-          delay(1000);      
-        }else tft.print("                    ");
-    }
+void logo(uint16_t color){
+  int dx = 100, dy = 0, w = 50, h = 75;
+  const int SZ = w * h / 8;
+  uint8_t sram[SZ];
+  
+  memcpy_P(sram, estrek, SZ);
+  tft.drawBitmap(dx, dy, estrek, w, h, color);   
+}
+
+void block_screen(){
+
+  logo(RED);
+
+  tft.setTextSize(2);
+  tft.setTextColor(RED);
+  tft.setCursor((tft.width() - 48) / 2, (tft.height() * 2) / 4);
+  tft.print("UNLOCK");
+  tft.setTextColor(YELLOW, BLACK);
+  tft.setCursor(0, (tft.height() * 6) / 8);
+  tft.print("Touch screen for loc");
+     
+  while(blocked){
+    tp = ts.getPoint();
+    pinMode(XM, OUTPUT);
+    pinMode(YP, OUTPUT);
+ 
+    if (tp.z < MINPRESSURE || tp.z > MAXPRESSURE) continue;
+    if (tp.x > 450 && tp.x < 570  && tp.y > 450 && tp.y < 570) blocked = false;
+    tft.setCursor(0, (tft.height() * 3) / 4);
+    tft.print("tp.x=" + String(tp.x) + " tp.y=" + String(tp.y) + "   ");    
+  }
+
+  tft.print(F("unlocked"));
+}
+
+boolean login(){
+  block_screen();
+  return true;
 }
 
 
-void setup(void)
-{
-    uint16_t tmp;
+void screen(){
+  logo(YELLOW);
+  button_exit();
+  button();
+  
+}
 
-    tft.reset();
-    ID = tft.readID();
-    tft.begin(ID);
-    Serial.begin(9600);
-    show_Serial();
-    tft.setRotation(Orientation);
-    tft.fillScreen(BLACK);
+void touch(){
+ while(!blocked){
+    tp = ts.getPoint();
+    pinMode(XM, OUTPUT);
+    pinMode(YP, OUTPUT);
+ 
+    if (tp.z < MINPRESSURE || tp.z > MAXPRESSURE) continue;
+    // EXIT button_exit();
+    if (tp.x > 450 && tp.x < 570  && tp.y > 450 && tp.y < 570) blocked = true;
+    // button() 
+    if (tp.x > 450 && tp.x < 870  && tp.y > 750 && tp.y < 845){
+    Keyboard.println("Your text here"); 
+    delay(1000);   
+    }   
+  }  
+}
 
+void setup(void){
+  uint16_t tmp;
 
-////////////////////////
-// Button
-///////////////////////
-    uint16_t cxpos = 20;
-    uint16_t cypos = 280;
-    uint16_t cradius = 20; 
-    uint16_t ccolor = CYAN;
-    tft.fillRect(cxpos, cypos-20, 100, 41, ccolor);
-    tft.fillCircle(cxpos, cypos, cradius, ccolor);
-    cxpos = cxpos + 100;
-    tft.fillCircle(cxpos, cypos, cradius, ccolor);
-    tft.setCursor(cxpos - 100, cypos);
-    tft.setTextSize(2);
-    tft.setTextColor(BLACK);
-    tft.print(F("Password"));  
+  tft.reset();
+  ID = tft.readID();
+  tft.begin(ID);
+  tft.setRotation(Orientation);
+  tft.fillScreen(BLACK);
 
-
-/////////////////////////////
-
-
-////////////////////////
-//    bitmap  
-//////////////////////// 
-
-    int dx = 100, dy, w = 50, h = 75;
-    const int SZ = w * h / 8;
-    uint8_t sram[SZ];
-    memcpy_P(sram, estrek, SZ);
-
-    dy = 0;
-    tft.drawBitmap(dx, dy, estrek, 50, 75, YELLOW); 
-    
-///////////////////////    
-
-    show_tft();
-       
 }
 
 void loop()
 {
-
+ tft.fillScreen(BLACK);
+ 
+ if(login()){
+    tft.fillScreen(BLACK);
+    screen();
+    touch();
+  }else{
+    // Error login()
+  }
 }
